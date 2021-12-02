526ba5159397105419a84c0a26a3bfa156f0ae61
prokopolja <kotejechka@gmail.com>
2021-12-02 11:09:36 +0300

    доб моб меню и порядок img в activities

--- js/main.js
+++ js/main.js
@@ -22,3 +22,9 @@ var reviewsSlider = new Swiper('.reviews-slider', {
   // Управление клавиатурой
   keyboard: true,
 });
+
+var menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener("click", function () {
+  console.log('Клик по кнопке меню');
+  document.querySelector('.navbar-bottom').classList.toggle('navbar-bottom--visible');
+});
